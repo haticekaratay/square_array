a3b9ee9455b9eceac2ac177acb5e7f43a89aeced
Hatice <haticekaraty@gmail.com>
2021-01-16 15:11:07 -0500

    commit tree 5b8ef449abc9d356dd56f735ca72e9ac7c42d504

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
 def square_array(array)
   # your code here
+  # iterate through each element in the array and return square of each element
+  squared_array=[]
+  array.each {|element| squared_array.push(element**2) }  
+  return squared_array
 end
